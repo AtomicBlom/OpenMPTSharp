dbd35cc9caea1c58d07012e4415611ecd4231e88
Steven Blom <atomicblom@binaryvibrance.net>
2025-08-03 22:35:59 +0930

    Support for tempo factor and infinite looping

--- OpenMPTSharp/Module.cs
+++ OpenMPTSharp/Module.cs
@@ -4,13 +4,25 @@ namespace OpenMPTSharp;
 
 public class Module : IDisposable
 {
-    private readonly unsafe OpenMPTFunctions.openmpt_module* _handle;
+	const string CTLPlayTempoFactor = "play.tempo_factor";
+
+	private readonly unsafe OpenMPTFunctions.openmpt_module* _handle;
+	private readonly string[] _supportedControls;
+    private bool _supportsTempoFactor;
 
     internal unsafe Module(OpenMPTFunctions.openmpt_module* handle)
     {
-        _handle = handle;
-        using ReturnedString controls = OpenMPTFunctions.openmpt_module_get_ctls(handle);
+	    _handle = handle;
+	    using ReturnedString controls = OpenMPTFunctions.openmpt_module_get_ctls(handle);
         using ReturnedString metadata = OpenMPTFunctions.openmpt_module_get_metadata_keys(handle);
+        _supportedControls = controls.String.Split(';');
+        foreach (var supportedControl in _supportedControls)
+        {
+	        switch (supportedControl)
+	        {
+		        case CTLPlayTempoFactor: _supportsTempoFactor = true; break;
+	        }
+        }
     }
     
     public unsafe void Dispose()
@@ -23,6 +35,43 @@ public class Module : IDisposable
     public unsafe int CurrentRow => OpenMPTFunctions.openmpt_module_get_current_row(_handle);
     public unsafe int ActiveChannels => OpenMPTFunctions.openmpt_module_get_current_playing_channels(_handle);
 
+    public unsafe double TempoFactor
+    {
+	    get
+	    {
+		    if (!_supportsTempoFactor) return 1;
+		    using StringToPass ctl = CTLPlayTempoFactor;
+		    var propertyResult = OpenMPTFunctions.openmpt_module_ctl_get_floatingpoint(_handle, ctl);
+		    using ReturnedString result = OpenMPTFunctions.openmpt_module_error_get_last_message(_handle);
+		    if (!string.IsNullOrEmpty(result))
+		    {
+			    throw new OpenMPTException(result);
+		    }
+
+		    return propertyResult;
+	    }
+	    set
+	    {
+		    if (!_supportsTempoFactor) return;
+		    using StringToPass ctl = CTLPlayTempoFactor;
+		    OpenMPTFunctions.openmpt_module_ctl_set_floatingpoint(_handle, ctl, value);
+		    using ReturnedString result = OpenMPTFunctions.openmpt_module_error_get_last_message(_handle);
+		    if (!string.IsNullOrEmpty(result))
+		    {
+			    throw new OpenMPTException(result);
+		    }
+	    }
+    }
+
+    public unsafe void RepeatEndlessly()
+    {
+	    OpenMPTFunctions.openmpt_module_set_repeat_count(_handle, -1);
+	    using ReturnedString result = OpenMPTFunctions.openmpt_module_error_get_last_message(_handle);
+	    if (!string.IsNullOrEmpty(result))
+	    {
+		    throw new OpenMPTException(result);
+	    }
+    }
 
     public unsafe int Read(int sampleRate, int length, Span<float> mono)
     {
